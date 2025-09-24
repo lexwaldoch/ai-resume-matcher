9dc3028ed54263d0390934d1a1b66050aff0d262
lexwaldoch <lexwaldoch@gmail.com>
2025-09-24 17:38:30 -0500

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,6 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import streamlit as st
 from models.resume_parser import extract_skills
 from models.job_matcher import calculate_match_score
