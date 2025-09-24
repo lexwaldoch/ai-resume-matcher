dc1d7f2c7da4d33ed0d3b9991d58482d04908e13
lexwaldoch <lexwaldoch@gmail.com>
2025-09-24 17:42:39 -0500

    Update resume_parser.py

--- models/resume_parser.py
+++ models/resume_parser.py
@@ -1,6 +1,11 @@
 import spacy
+import os
 
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    os.system("python -m spacy download en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 def extract_skills(text):
     doc = nlp(text)
